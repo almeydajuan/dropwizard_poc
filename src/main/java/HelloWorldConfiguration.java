7f293dd8d631031aff88f7439abdb0e9732b6a03
Juan Almeyda <almeydajuan@gmail.com>
2016-05-13 18:10:27 -0300

    First commit

---
+++ src/main/java/HelloWorldConfiguration.java
@@ -0,0 +1,35 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ * Created by Juan Almeyda on 5/13/2016.
+ */
+public class HelloWorldConfiguration extends Configuration {
+
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
+}
